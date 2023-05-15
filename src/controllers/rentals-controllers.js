18dfe750b22e97e5a8aa94a76a7461024bb118d0
suelen <sreginaclsilva@gmail.com>
2023-05-15 01:27:19 -0300

    feat: add controller to POST new rental

---
+++ src/controllers/rentals-controllers.js
@@ -0,0 +1,48 @@
+import dayjs from "dayjs"
+import { db } from "../database/database.js"
+
+export async function getRentals(req, res){
+    try {
+        
+    } catch (error) {
+        res.status(500).send(error.message)
+    }
+}
+
+export async function postNewRental(req, res){
+    const {customerId, gameId, daysRented} = req.body
+    try {
+        const searchValidRental = await db.query(`
+        SELECT "gameId", "customerId" FROM rentals
+            JOIN games ON games.id=$1
+            JOIN customers ON customers.id=$2`, [gameId, customerId])
+        if(!searchValidRental.rowCount) return res.sendStatus(400)
+        const availableStock = await db.query(`
+        SELECT * FROM games WHERE games.id=$1
+            AND (SELECT COUNT(*) FROM rentals WHERE rentals."gameId"=$1 AND rentals."returnDate" IS NULL) < games."stockTotal"`, [gameId])
+        if(!availableStock.rowCount) return res.sendStatus(400)
+        const date = dayjs().format("YYYY-MM-DD")
+        await db.query(`INSERT INTO rentals ("gameId", "customerId", "rentDate", "daysRented", "returnDate", "originalPrice", "delayFee")
+        SELECT $1, $2, $4, $3, null, games."pricePerDay" * $3, null FROM games WHERE games.id=$1;`, [gameId, customerId, daysRented, date])
+        res.send(201)
+        
+    } catch (error) {
+        res.status(500).send(error.message)
+    }
+}
+
+export async function finishRental(req, res){
+    try {
+        
+    } catch (error) {
+        res.status(500).send(error.message)
+    }
+}
+
+export async function deleteRental(req, res){
+    try {
+        
+    } catch (error) {
+        res.status(500).send(error.message)
+    }
+}
--- src/routes/rentals.routes.js
+++ src/routes/rentals.routes.js
@@ -1,10 +1,13 @@
-import { Router } from "express";
+import { Router } from "express"
+import { deleteRental, finishRental, getRentals, postNewRental } from "../controllers/rentals-controllers.js"
+import rentalSchema from "../schemas/rentals.schema.js"
+import validateSchema from "../middlewares/validateSchema.js"
 
 const rentalsRoutes = Router()
 
-rentalsRoutes.get("/rentals")
-rentalsRoutes.post("/rentals")
-rentalsRoutes.post("/rentals/:id/return")
-rentalsRoutes.delete("/rentals/:id")
+rentalsRoutes.get("/rentals", getRentals)
+rentalsRoutes.post("/rentals", validateSchema(rentalSchema), postNewRental)
+rentalsRoutes.post("/rentals/:id/return", finishRental)
+rentalsRoutes.delete("/rentals/:id", deleteRental)
 
 export default rentalsRoutes
